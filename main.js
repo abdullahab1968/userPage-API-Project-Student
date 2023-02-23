f42ced51f9225f3bf6b8ed3b52afbb02ede4524a
Abdallah Abu khdair <71616389+abdullahab1968@users.noreply.github.com>
2023-02-23 14:31:40 +0200

    generate user button implementation

--- main.js
+++ main.js
@@ -1,6 +1,21 @@
-const api = new APIManager()
+let api = new APIManager()
 api.randomUserAPI()
 api.randomKanyeQuoteAPI()
 api.pokeAPI()
 api.baconIpsumAPI()
 // console.log(api.data)
+$('#generate-button').on("click", function(){
+    $('.user-container').empty()
+    $('.quote-container').empty()
+    $('.pokemon-container').empty()
+    $('.meat-container').empty()
+    $('.friends-container').empty()
+    
+
+
+    api = new APIManager()
+    api.randomUserAPI()
+    api.randomKanyeQuoteAPI()
+    api.pokeAPI()
+    api.baconIpsumAPI()
+})
